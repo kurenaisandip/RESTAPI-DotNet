f8b53fee3068488bdd4af8dfe775ba558f5c4dbe
Sandip <srai68141@gmail.com>
2024-07-30 21:23:38 +0545

    16 Defining the movie API contracts

---
+++ 1.GettingStarted/Movies.Contracts/Requests/CreateMovieRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Movies.Contracts.Requests;
+
+public class CreateMovieRequest
+{
+    public required string Title { get; init; }
+
+    public required int YearofRelease { get; init; }
+    
+    //an array of strings with default value of empty string
+    public required IEnumerable<string> Genre { get; init; } = Enumerable.Empty<string>();
+}
---
+++ 1.GettingStarted/Movies.Contracts/Responses/MovieResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Movies.Contracts.Responses;
+
+public class MovieResponse
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; init; }
+
+    public required int YearofRelease { get; init; }
+    
+    //an array of strings with default value of empty string
+    public required IEnumerable<string> Genre { get; init; } = Enumerable.Empty<string>();
+}
